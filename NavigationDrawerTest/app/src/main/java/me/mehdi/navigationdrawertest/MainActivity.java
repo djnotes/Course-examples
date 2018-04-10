55ff7461e8e9f196b6d0fec68b2013b47dad4ef7
jasonMisaq <powergame_coder2@yahoo.com>
2018-04-10 23:58:48 +0430

    First commit

---
+++ NavigationDrawerTest/app/src/main/java/me/mehdi/navigationdrawertest/MainActivity.java
@@ -0,0 +1,34 @@
+package me.mehdi.navigationdrawertest;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+    }
+}
